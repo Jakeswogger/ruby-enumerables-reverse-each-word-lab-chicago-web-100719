dfa2a04d40975d4966f935b6181c2ee49136562b
Jake Swogger <jakeswogger1@gmail.com>
2019-09-18 18:49:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence) 
   betterSentence = sentence.split()
-  betterSentence.collect { |b| b.reverse }
+  final = betterSentence.collect { |b| b.reverse }
   betterSentence
   betterSentence
 end
